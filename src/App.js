9c00f906ed5683668d3002c2c6a83b9808feed3c
Mohamed Abraham CHERIF <cherifmohamedabraham@gmail.com>
2024-08-23 18:15:15 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: 'CHERIF Mohamed Abraham',
+        bio: 'Développeur passionné et étudiant chez GoMyCode',
+        imgSrc: '/avatar.png',
+        profession: 'Développeur Web',
+      },
+      shows: false,
+      timeSinceMounted: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState((prevState) => ({
+        timeSinceMounted: prevState.timeSinceMounted + 1,
+      }));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  toggleShow = () => {
+    this.setState((prevState) => ({
+      shows: !prevState.shows,
+    }));
+  };
+
+  render() {
+    const { person, shows, timeSinceMounted } = this.state;
+
+    return (
+      <div className="App">
+        <button onClick={this.toggleShow}>
+          {shows ? 'Cacher le Profil' : 'Montrer le Profil'}
+        </button>
+        {shows && (
+          <div>
+            <h1>{person.fullName}</h1>
+            <img src={person.imgSrc} alt="Profil" />
+            <p>{person.bio}</p>
+            <h3>{person.profession}</h3>
+          </div>
+        )}
+        <p>Temps écoulé depuis le montage : {timeSinceMounted} secondes</p>
+      </div>
+    );
+  }
 }
 
 export default App;
